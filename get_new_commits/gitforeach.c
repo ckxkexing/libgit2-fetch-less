1e6b11bc2055a6af329beb588da78aea0c80a250
ckxkexing <1585799755@qq.com>
2023-03-09 22:01:34 +0800

    修好了4个object的遍历抽取，之前是tree的问题，tree size = 0

--- get_new_commits/gitforeach.c
+++ get_new_commits/gitforeach.c
@@ -14,13 +14,19 @@ static git_repository *_repo;
 
 struct exhaustive_state {
 	git_odb *db;
-	git_vector commits;
+    int cnt;
+    int commit_cnt;
+    int blob_cnt;
 };
 
 /** Get all commits from the repository. */
 static int exhaustive_objs(const git_oid *id, void *payload)
 {
+    char shortsha[41] = {0};
+    git_oid_tostr(shortsha, 40, id);
 	struct exhaustive_state *mc = (struct exhaustive_state *)payload;
+    mc->cnt += 1;
+    printf(">>>>>>>>>>>>>>>>>>>>>>>%d\n", mc->cnt);
 	size_t header_len;
 	git_object_t header_type;
     long size;
@@ -28,8 +34,8 @@ static int exhaustive_objs(const git_oid *id, void *payload)
     git_odb_object *obj;
 	int error = 0;
 	error = git_odb_read_header(&header_len, &header_type, mc->db, id);
+    printf("check type %d for  %s\n", header_type, shortsha);
 	if (header_type == GIT_OBJECT_COMMIT) {
-        puts("commit");
         if(git_odb_read(&obj, mc->db, id) < 0)
             goto end;
         data = (const unsigned char *)git_odb_object_data(obj);
@@ -38,72 +44,57 @@ static int exhaustive_objs(const git_oid *id, void *payload)
         git_object_free(obj);
 	}
     else if(header_type == GIT_OBJECT_TREE) {
-        puts("tree");
-        git_tree *tree = NULL;
-        git_tree_lookup(&tree, _repo, id);
-        git_treebuilder *tb = NULL;
-        git_treebuilder_new(&tb, _repo, tree);
-        git_treebuilder_write_cache(id, tb);
-        error = git_rocks_odb_write(id, mc->db, tb->write_cache.ptr, tb->write_cache.size , GIT_OBJECT_TREE);
-        git_treebuilder_free(tb);
+        git_odb_read(&obj, mc->db, id);
+        data = (const unsigned char *)git_odb_object_data(obj);
+        size = (long)git_odb_object_size(obj);
+        error = git_rocks_odb_write(id, mc->db, data, size, GIT_OBJECT_TREE);
+        git_object_free(obj);
     }
     else if(header_type == GIT_OBJECT_BLOB){
-        puts("blob");
         git_blob *blob = NULL;
-        if(git_blob_lookup(&blob, _repo, id) < 0)
+        if(git_blob_lookup(&blob, _repo, id) < 0){
             goto end;
+        }
         size = (long)git_blob_rawsize(blob);
         const void * blob_rawcontent = git_blob_rawcontent(blob);
         error = git_rocks_odb_write(id, mc->db, blob_rawcontent, size, GIT_OBJECT_BLOB);
         git_blob_free(blob);
     }
     else if(header_type == GIT_OBJECT_TAG){
-        return 0;
-        puts("tag");
         if(git_odb_read(&obj, mc->db, id) < 0)
             goto end;
         data = (const unsigned char *)git_odb_object_data(obj);
         size = git_odb_object_size(obj);
-        error = git_rocks_odb_write(id, mc->db, data, size, GIT_OBJECT_COMMIT);
+        error = git_rocks_odb_write(id, mc->db, data, size, GIT_OBJECT_TAG);
         git_object_free(obj);
     }
 
-    end:
+end:
     if(error < 0) {
+        printf("error = %d, type = %d\n", error, header_type);
         char shortsha[41] = {0};
         git_oid_tostr(shortsha, 40, id);
         printf("error id %s\n", shortsha);
         printf("error raw =\n");
         p_write(fileno(stdout), data, (size_t)size);
         puts("");
+        // int c = 2 / 0;
+        return error;
     }
-	return 0;
-}
 
-/** 根据tree需要单独遍历保存 */
-
-/** Get all reference from the repository. */
-static int exhaustive_refs(const git_oid *id, void *payload) {
     return 0;
 }
 
-static int foreach_cb(const git_oid *oid, void *data)
-{
-	int *nobj = data;
-	(*nobj)++;
-
-	// GIT_UNUSED(oid);
-    printf("%d %s\n", *nobj, git_oid_tostr_s(oid));
-
-	return 0;
-}
 
 int main(){
     git_libgit2_init();
 
 	int nobj = 0;
     int error = 0;
-    const char *path = "../tmp_2023";
+    // const char *path = "../tmp_2023";
+    // const char *path = "../diffusers";
+    const char *path = "../cargo-c-rev";
+
 	error = git_repository_open(&_repo, path);
     if(error) {
         puts("repo error");
@@ -114,8 +105,6 @@ int main(){
         puts("odb error");
         return 0;
     }
-    // git_odb_foreach(_odb, foreach_cb, &nobj);
-    // printf("%d\n", nobj);
 
     /*
      * 遍历获取Objects信息
@@ -123,12 +112,15 @@ int main(){
      * */
     struct exhaustive_state mc = {
             .db = NULL,
-            .commits = GIT_VECTOR_INIT,
+            .cnt = 0,
+            .commit_cnt=0,
+            .blob_cnt=0
     };
 
     git_repository_odb(&mc.db, _repo);
+    printf("object cnt = %d\n", mc.cnt);
     git_odb_foreach(mc.db, &exhaustive_objs, &mc);
-
+    printf("object cnt = %d\n", mc.cnt);
     git_libgit2_shutdown();
     return 0;
 }
